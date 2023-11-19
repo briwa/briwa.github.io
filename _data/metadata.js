09ba51ac7ddb0dbfd642dec7e53c6550994241cd
briwa <hello@briwa.dev>
2023-11-19 15:43:42 +0900

    feat: use eleventy's own starter pack for blog

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "Briwa",
+	url: "https://briwa.dev/",
+	language: "en",
+	description: "briwa.dev",
+	author: {
+		name: "briwa",
+		email: "hello@briwa.dev",
+		url: "https://briwa.dev/about"
+	}
+}
